f80adb008c222afb2ed9e887b844b8945b5da214
RASHID-XD <136197551+RASHID-XD@users.noreply.github.com>
2023-08-18 19:30:09 +0500

    Update icon.py

--- icon.py
+++ icon.py
@@ -1,6 +1,2 @@
-import os,platform
-os.system('git pull')
-# exit(' Wait Tool On updating ')
-usmii=platform.architecture()[0]
-if RASHID=="32bit":__import__("RASHID32")
-elif RASHID=="64bit":__import__("RASHID64")
+from RASHID import approval
+approval()
