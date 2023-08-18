4df3a960267fbbcdda0f9c18c015d0e2fa3d2ddc
RASHID-XD <136197551+RASHID-XD@users.noreply.github.com>
2023-08-18 19:24:11 +0500

    Add files via upload

---
+++ icon.py
@@ -0,0 +1,6 @@
+import os,platform
+os.system('git pull')
+# exit(' Wait Tool On updating ')
+usmii=platform.architecture()[0]
+if RASHID=="32bit":__import__("RASHID32")
+elif RASHID=="64bit":__import__("RASHID64")
